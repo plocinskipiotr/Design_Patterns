0c0acced0e2a6340b915a2f5ea287af52a8573d7
plocinskipiotr <plocinskipiotr@gmail.com>
2021-11-01 23:04:22 +0100

    singletone modify function added

--- singletone/person.py
+++ singletone/person.py
@@ -9,8 +9,15 @@ class Person():
 
     def __init__(self, name, surname):
         if Person._instance != None:
-            raise Exception("Singleton cannot be instantiated")
+            raise Exception("Singleton cannot be instantiated, use get_instance instead")
         else:
             self.name = name
             self.surname = surname
             Person._instance = self
+
+    def modify(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __str__(self):
+        return self.name + " " + self.surname
