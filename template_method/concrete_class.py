acb6fae152202159076aa4de95c9387aeee91e30
plocinskipiotr <plocinskipiotr@gmail.com>
2021-12-30 20:33:48 +0100

    template method added

---
+++ template_method/concrete_class.py
@@ -0,0 +1,12 @@
+"""Concrete class"""
+
+from abstract_class import AbstractClass
+
+
+class ConcreteClass(AbstractClass):
+    """Concrete template class"""
+
+    @staticmethod
+    def step_3():
+        """Concrete step of algorithm"""
+        print('concrete step_3')
